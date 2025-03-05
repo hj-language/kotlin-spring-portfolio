9aa61becf5246627120fad2c3009110cec4a2ebd
Hyejin Kim <idgafff010@gmail.com>
2025-03-06 01:59:28 +0900

    엔티티에 기본 필드 추가 - 연관관계 없는 엔티티
    Skill.kt
    - type은 지난 시간에 만든 SkillType Enum 추가
    - 생성자에서는 문자열로 받고, 이를 기반으로 enum 값으로 변환하여 저장
    - type 처럼 예약어로 사용될 수 있는 단어는 컬럼명으로 사용 지양 -> skill_type으로 지정

--- src/main/kotlin/com/hyejin/portfolio/domain/entity/Introduction.kt
+++ src/main/kotlin/com/hyejin/portfolio/domain/entity/Introduction.kt
@@ -1,17 +1,19 @@
 package com.hyejin.portfolio.domain.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
 @Entity
-class Introduction : BaseEntity() {
+class Introduction(
+        content: String,
+        isActive: Boolean,
+) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "introduction_id")
     var id: Long? = null
 
+    var content: String = content
+
+    var isActive: Boolean = isActive
 }
--- src/main/kotlin/com/hyejin/portfolio/domain/entity/Skill.kt
+++ src/main/kotlin/com/hyejin/portfolio/domain/entity/Skill.kt
@@ -1,17 +1,25 @@
 package com.hyejin.portfolio.domain.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import com.hyejin.portfolio.domain.contant.SkillType
+import jakarta.persistence.*
 
 @Entity
-class Skill : BaseEntity() {
+class Skill(
+        name: String,
+        type: String,
+        isActive: Boolean
+) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "skill_id")
     var id: Long? = null
 
+    var name: String = name
+
+    @Column(name = "skill_type")
+    @Enumerated(EnumType.STRING)
+    var type: SkillType = SkillType.valueOf(type)
+
+    var isActive: Boolean = isActive
 }
