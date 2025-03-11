c1d41039e870c4ffbcdfb499b5d224480ea821e8
Hyejin Kim <idgafff010@gmail.com>
2025-03-12 03:15:04 +0900

    인터셉터 개발

--- src/main/kotlin/com/hyejin/portfolio/domain/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/hyejin/portfolio/domain/presentation/interceptor/PresentationInterceptor.kt
@@ -1,7 +1,18 @@
 package com.hyejin.portfolio.domain.presentation.interceptor
 
+import com.hyejin.portfolio.domain.entity.HttpInterface
+import com.hyejin.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
 
 @Component
-class PresentationInterceptor {
+class PresentationInterceptor(
+        private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
+    override fun afterCompletion(request: HttpServletRequest, response: HttpServletResponse, handler: Any, ex: Exception?) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
 }
