c3b9a5634c6aacba6d28662a406d4923a2a53ae2
Hyejin Kim <idgafff010@gmail.com>
2025-03-23 21:03:29 +0900

    삽입, 수정, 조회 API 개발 - 연관관계 일대다

--- src/main/kotlin/com/hyejin/portfolio/admin/context/experience/service/AdminExperienceService.kt
+++ src/main/kotlin/com/hyejin/portfolio/admin/context/experience/service/AdminExperienceService.kt
@@ -1,11 +1,13 @@
 package com.hyejin.portfolio.admin.context.achievement.service
 
+import com.hyejin.portfolio.admin.context.experience.form.ExperienceForm
 import com.hyejin.portfolio.admin.data.TableDTO
 import com.hyejin.portfolio.admin.exception.AdminBadRequestException
 import com.hyejin.portfolio.domain.entity.Experience
 import com.hyejin.portfolio.domain.entity.ExperienceDetail
 import com.hyejin.portfolio.domain.repository.ExperienceRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class AdminExperienceService(
@@ -15,7 +17,7 @@ class AdminExperienceService(
         return TableDTO.from(Experience::class, experienceRepository.findAll(), "details")
     }
 
-    fun getExperienceTable(id: Long?): TableDTO {
+    fun getExperienceDetailTable(id: Long?): TableDTO {
         val entities = if (id != null)
             experienceRepository.findById(id)
                 .orElseThrow { throw AdminBadRequestException("ID ${id}에 해당하는 데이터를 찾을 수 없습니다.") }
@@ -23,5 +25,45 @@ class AdminExperienceService(
         else emptyList()
 
         return TableDTO.from(ExperienceDetail::class, entities)
+    }
+
+    @Transactional
+    fun save(form: ExperienceForm) {
+        val experienceDetails = form.details
+            ?.map { detail -> detail.toEntity() }
+            ?.toMutableList()
+        val experience = form.toEntity()
+        experience.addDetails(experienceDetails)
+
+        experienceRepository.save(experience)
+    }
+
+    @Transactional
+    fun update(id: Long, form: ExperienceForm) {
+        val experience = experienceRepository.findById(id)
+            .orElseThrow { throw AdminBadRequestException("ID ${id}에 해당하는 데이터를 찾을 수 없습니다.") }
+
+        experience.update(
+            title = form.title,
+            description = form.description,
+            startYear = form.startYear,
+            startMonth = form.startMonth,
+            endYear = form.endYear,
+            endMonth = form.endMonth,
+            isActive = form.isActive
+        )
+
+        val detailMap = experience.details.map { it.id to it }.toMap()
+        form.details?.forEach {
+            val entity = detailMap.get(it.id)
+            if (entity != null) {
+                entity.update(
+                    content = it.content,
+                    isActive = it.isActive
+                )
+            } else {
+                experience.details.add(it.toEntity())
+            }
+        }
     }
 }
